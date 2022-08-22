3025f2cc4c029980217abeb5ac40a5baf8ead60e
Ivelin Iliev <73820008+ivooo369@users.noreply.github.com>
2022-08-22 22:31:26 +0300

    Add some styling

--- app.js
+++ app.js
@@ -15,6 +15,9 @@ function game() {
   let counterPointsPlayer = 0;
   let counterPointsComputer = 0;
   let gameWinner = "";
+  let playerWinner = false;
+  let computerWinner = false;
+  let tie = false;
 
   // Add event listeners for all three buttons
   const buttons = document.querySelectorAll("button");
@@ -33,25 +36,53 @@ function game() {
   function playRound(playerSelection, computerSelection) {
     if (playerSelection === "rock" || playerSelection === "paper" || playerSelection === "scissors") {
       if (playerSelection === computerSelection) {
+        tie = true;
+        playerWinner = false;
+        computerWinner = false;
+        colorWinner();
         return `It's a Tie! Your choice is ${playerSelection} and the computer's choice is ${computerSelection}.`;
       } else if (playerSelection === "rock" && computerSelection === "paper") {
         counterPointsComputer++;
-        return `You Lose! Your choice is ${playerSelection} and the computer's choice is' ${computerSelection}.`;
+        computerWinner = true;
+        playerWinner = false;
+        tie = false;
+        colorWinner();
+        return `You Lose! Your choice is ${playerSelection} and the computer's choice is ${computerSelection}.`;
       } else if (playerSelection === "paper" && computerSelection === "scissors") {
         counterPointsComputer++;
-        return `You Lose! Your choice is ${playerSelection} and the computer's choice is' ${computerSelection}.`;
+        computerWinner = true;
+        playerWinner = false;
+        tie = false;
+        colorWinner();
+        return `You Lose! Your choice is ${playerSelection} and the computer's choice is ${computerSelection}.`;
       } else if (playerSelection === "scissors" && computerSelection === "rock") {
         counterPointsComputer++;
-        return `You Lose! Your choice is ${playerSelection} and the computer's choice is' ${computerSelection}.`;
+        computerWinner = true;
+        playerWinner = false;
+        tie = false;
+        colorWinner();
+        return `You Lose! Your choice is ${playerSelection} and the computer's choice is ${computerSelection}.`;
       } else if (playerSelection === "rock" && computerSelection === "scissors") {
         counterPointsPlayer++;
-        return `You Win! Your choice is ${playerSelection} and the computer's choice is' ${computerSelection}.`;
+        playerWinner = true;
+        computerWinner = false;
+        tie = false;
+        colorWinner();
+        return `You Win! Your choice is ${playerSelection} and the computer's choice is ${computerSelection}.`;
       } else if (playerSelection === "paper" && computerSelection === "rock") {
         counterPointsPlayer++;
-        return `You Win! Your choice is ${playerSelection} and the computer's choice is' ${computerSelection}.`;
+        playerWinner = true;
+        computerWinner = false;
+        tie = false;
+        colorWinner();
+        return `You Win! Your choice is ${playerSelection} and the computer's choice is ${computerSelection}.`;
       } else if (playerSelection === "scissors" && computerSelection === "paper") {
         counterPointsPlayer++;
-        return `You Win! Your choice is ${playerSelection} and the computer's choice is' ${computerSelection}.`;
+        playerWinner = true;
+        computerWinner = false;
+        tie = false;
+        colorWinner();
+        return `You Win! Your choice is ${playerSelection} and the computer's choice is ${computerSelection}.`;
       }
       return declareWinner;
     }
@@ -72,31 +103,42 @@ function game() {
   // create div DOM for all results
   const container = document.querySelector("#container");
   const resultsDiv = document.createElement("div");
+  resultsDiv.classList.add("results");
   resultsDiv.style.marginTop = "20px";
   container.appendChild(resultsDiv);
 
   // create player win tracking DOM
-  const playerWinText = document.createElement("p");
-  playerWinText.style.color = "blue";
+  const playerWinText = document.createElement("span");
+  playerWinText.style.color = "#00ff00";
   playerWinText.textContent = "Player Score: " + counterPointsPlayer;
   resultsDiv.appendChild(playerWinText);
 
   // create computer win tracking DOM
-  const computerWinText = document.createElement("p");
-  computerWinText.style.color = "blue";
+  const computerWinText = document.createElement("span");
+  computerWinText.style.color = "#ff0000";
   computerWinText.textContent = "Computer Score: " + counterPointsComputer;
   resultsDiv.appendChild(computerWinText);
 
   // create battle win text DOM
   const battleWinText = document.createElement("p");
-  battleWinText.style.color = "black";
+  battleWinText.style.color = "red";
   resultsDiv.appendChild(battleWinText);
 
   // create game win text DOM
   const gameWinText = document.createElement("p");
-  gameWinText.style.color = "orange";
+  gameWinText.style.color = "#ffd700";
   gameWinText.textContent = gameWinner;
   resultsDiv.appendChild(gameWinText);
+
+  function colorWinner() {
+    if (playerWinner === true) {
+      battleWinText.style.color = "green";
+    } else if (computerWinner === true) {
+      battleWinText.style.color = "red";
+    } else {
+      battleWinText.style.color = "white";
+    }
+  }
 
   // determine who won to five points first
   function endGame() {
@@ -124,8 +166,11 @@ function game() {
 
       // disable game buttons
       document.getElementById("1").disabled = true;
+      document.getElementById("1").style.pointerEvents = "none";
       document.getElementById("2").disabled = true;
+      document.getElementById("2").style.pointerEvents = "none";
       document.getElementById("3").disabled = true;
+      document.getElementById("3").style.pointerEvents = "none";
 
       // create new DOM button to replay
       const playAgainButton = document.createElement("button");
